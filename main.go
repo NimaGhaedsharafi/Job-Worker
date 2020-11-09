86e4278dee1e594a4b881b26da5ffd967095c8e8
Nima Ghaedsharafi <nima.ghaedsharafi@snapp.cab>
2020-11-09 20:01:35 +0330

    scaffolds main

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "coroner/config"
+import (
+	"coroner/config"
+	"coroner/worker"
+)
 
 func main() {
 	config.Init()
+
+	dispatcher := worker.NewDispatcher(config.Cfg.Dispatcher.MaxWorker)
+	dispatcher.Run()
+
+	// TODO: add close functionality to dispatcher
+
+	// fetch drivers
+
+	// loop through all drivers and dispatch a job
+
+	// once everything is done, create a report in csv
+
+	// send an email
 }
