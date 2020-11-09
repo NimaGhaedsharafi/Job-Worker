8d2ec90795062e4e4c4790b936fc1eaeff79c417
Nima Ghaedsharafi <nima.ghaedsharafi@snapp.cab>
2020-11-09 20:09:08 +0330

    adds database to main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"coroner/config"
+	"coroner/database"
 	"coroner/worker"
 )
 
@@ -10,10 +11,11 @@ func main() {
 
 	dispatcher := worker.NewDispatcher(config.Cfg.Dispatcher.MaxWorker)
 	dispatcher.Run()
-
 	// TODO: add close functionality to dispatcher
 
 	// fetch drivers
+	database.Init()
+	defer database.Close()
 
 	// loop through all drivers and dispatch a job
 
